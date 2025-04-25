4ad4bb80d7bb849443798236248e5de93c15651a
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-04-25 11:19:59 +0100

    go

--- script/pondiverse.js
+++ script/pondiverse.js
@@ -1,1 +1,8 @@
-export async function fetchCreations() {}
+export async function fetchCreations() {
+  const response = await fetch(
+    "https://todepond--8d30672821b811f0b0cd569c3dd06744.web.val.run"
+  );
+  const json = await response.json();
+  if (!json.ok) throw new Error("Failed to fetch creations");
+  return json.rows;
+}
