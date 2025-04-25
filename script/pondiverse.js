d6a7248c8003dad639a22d4f1c05656cead08b49
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-04-25 11:41:22 +0100

    admin

--- script/pondiverse.js
+++ script/pondiverse.js
@@ -6,3 +6,12 @@ export async function fetchCreations() {
   if (!json.ok) throw new Error("Failed to fetch creations");
   return json.rows;
 }
+
+export async function updateDatabase() {
+  const response = await fetch(
+    `https://todepond--d60ba2a421b911f09a39569c3dd06744.web.val.run`
+  );
+  const json = await response.json();
+  if (!json.ok) throw new Error("Failed to update database");
+  return json;
+}
