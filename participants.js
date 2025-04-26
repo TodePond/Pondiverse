0a7191215f9a335f7721ff993b0636855d0f61be
mmct <pprt4@nottingham.ac.uk>
2025-04-26 11:30:35 +0100

    Added guide and load in participants from participants.js  - Wrote a guide for how to participate in the Pondiverse  - Put a list of participants in participants.js and edited the create    and explore pages to use this list for getting links

---
+++ participants.js
@@ -0,0 +1,5 @@
+export const participants = {
+      "screenpond": {name: "Screenpond", url: "https://screenpond.cool/"},
+      "chaoskit": {name: "Chaoskit", url: "https://evolved.systems/chaoskit/"},
+      "pnmrsimjs": {name: "pNMRsimJS", url: "https://mags.omg.lol/simulator.php"},
+    };
