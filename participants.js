7a0fbe2912cc18b65bb3d36a6dfeb24feb5b7990
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-04-27 08:28:59 +0100

    oi

--- participants.js
+++ participants.js
@@ -1,6 +1,22 @@
-export const participants = {
-      "screenpond": {url: "https://screenpond.cool/"},
-      "chaoskit": {url: "https://evolved.systems/chaoskit/"},
-      "pnmrsimjs": {url: "https://mags.omg.lol/simulator.php"},
-      "codepond": {url: "https://codepond.elouan.xyz/"},
-    };
+const participants = {
+  screenpond: { url: "https://screenpond.cool/" },
+  chaoskit: { url: "https://evolved.systems/chaoskit/" },
+  pnmrsimjs: { url: "https://mags.omg.lol/simulator.php" },
+  codepond: { url: "https://codepond.elouan.xyz/" },
+  cherry: { url: "https://cherry.cthulahoops.org/" },
+  bitart: { url: "https://iliazeus.lol/bitart/" },
+};
+
+export function getTypeUrl(type) {
+  type = type.toLowerCase();
+  return participants[type]?.url;
+}
+
+export function getTypes() {
+  const types = [];
+  for (const type in participants) {
+    const participant = participants[type];
+    types.push({ name: type, url: participant.url });
+  }
+  return types;
+}
