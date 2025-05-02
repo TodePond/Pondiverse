0c94c6c078235f51032148f1463964a9eea0d79a
Ilia Pozdnyakov <iliazeus@proton.me>
2025-05-02 22:38:21 +0500

    add homepages to instances.js

--- instances.js
+++ instances.js
@@ -1,6 +1,7 @@
 export const instances = [
   {
     name: "todepondiverse",
+    homepage: "https://pondiverse.com",
     addCreation: "https://pondiverse.val.run/add-creation",
     getCreation: "https://pondiverse.val.run/get-creation?id=",
     getCreationImage: "https://pondiverse.val.run/get-creation-image?id=",
@@ -8,6 +9,7 @@ export const instances = [
   },
   {
     name: "puddle",
+    homepage: "https://iliazeus.lol/puddle/",
     addCreation: "https://iliazeus-puddle.web.val.run/creations/",
     getCreation: "https://iliazeus-puddle.web.val.run/creations/",
     // getCreationImage: "TODO", // i have images as data URIs inside creations currently
