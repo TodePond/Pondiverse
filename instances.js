53a472905f5dcc125dd3e6208ebfd9e6836a1baf
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-05-02 16:32:10 +0100

    names and domains?

--- instances.js
+++ instances.js
@@ -1,13 +1,15 @@
 export const instances = [
   {
-    name: "pondiverse.com",
+    name: "todepondiverse",
+    domain: "pondiverse.com",
     addCreation: "https://pondiverse.val.run/add-creation",
     getCreation: "https://pondiverse.val.run/get-creation/?id=",
     getCreationImage: "https://pondiverse.val.run/get-creation-image/?id=",
     getCreations: "https://pondiverse.val.run/get-creations",
   },
   {
-    name: "iliazeus.lol/puddle",
+    name: "puddle",
+    domain: "iliazeus.lol",
     addCreation: "https://iliazeus-puddle.web.val.run/creations/",
     getCreation: "https://iliazeus-puddle.web.val.run/creations/",
     // getCreationImage: "TODO", // i have images as data URIs inside creations currently
