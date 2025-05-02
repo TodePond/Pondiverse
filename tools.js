1a1d5e9e28c9af93ad9b351bbb8524d1321d8852
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-05-02 14:58:12 +0100

    types types tpyes tpyes

---
+++ tools.js
@@ -0,0 +1,47 @@
+export const tools = [
+  {
+    name: "screendpond",
+    types: ["screenpond"],
+    url: "https://screendpond.cool?creation=",
+  },
+  {
+    name: "chaoskit",
+    types: [],
+    url: "https://evolved.systems/chaoskit/",
+  },
+  {
+    name: "pnmrsimjs",
+    types: [],
+    url: "https://mags.omg.lol/simulator.php",
+  },
+  {
+    name: "codepond",
+    types: [],
+    url: "https://codepond.elouan.xyz/",
+  },
+  {
+    name: "cherry",
+    types: [],
+    url: "https://cherry.cthulahoops.org/",
+  },
+  {
+    name: "collapse",
+    types: ["collapse"],
+    url: "https://collapse.cthulahoops.org/?creation=",
+  },
+  {
+    name: "bitart",
+    type: ["bitart"],
+    url: "https://iliazeus.lol/bitart/?creation=",
+  },
+  {
+    name: "real shaders",
+    types: ["real-shader"],
+    url: "https://garten.salat.dev/real-shaders.html?creation=",
+  },
+  {
+    name: "fake shaders II",
+    types: ["fake-shader"],
+    url: "https://garten.salat.dev/fake-shaders.html?creation=",
+  },
+];
