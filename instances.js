3d73e40b02107085ffb2fe7eb049e707b6870773
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-05-02 15:36:19 +0100

    gogo

---
+++ instances.js
@@ -0,0 +1,13 @@
+export const instances = [
+  {
+    name: "pondiverse.com",
+    addCreation: "https://pondiverse.val.run/add-creation",
+    getCreation: "https://pondiverse.val.run/get-creation/?id=",
+    getCreationImage: "https://pondiverse.val.run/get-creation-image/?id=",
+    getCreations: "https://pondiverse.val.run/get-creations",
+  },
+  {
+    name: "iliazeus.lol",
+    // todo
+  },
+];
