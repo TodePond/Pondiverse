95b33c4927eeffdc70727f4c6d3bf5482f142e54
Ilia Pozdnyakov <iliazeus@proton.me>
2025-05-03 03:37:42 +0500

    fix the button

--- instances.js
+++ instances.js
@@ -7,12 +7,12 @@ export const instances = [
     getCreationImage: "https://pondiverse.val.run/get-creation-image?id=",
     getCreations: "https://pondiverse.val.run/get-creations",
   },
-  //{
-  //  name: "puddle",
- //   home: "https://iliazeus.lol/puddle/",
- //   addCreation: "https://api.iliazeus.lol/puddle/creations",
-  //  getCreation: "https://api.iliazeus.lol/puddle/creations/",
-  //  // getCreationImage: "TODO", // i have images as data URIs inside creations currently
-  //  getCreations: "https://api.iliazeus.lol/puddle/creations",
- // },
+  {
+    name: "puddle",
+    home: "https://iliazeus.lol/puddle/",
+    addCreation: "https://api.iliazeus.lol/puddle/creations",
+    getCreation: "https://api.iliazeus.lol/puddle/creations/",
+    // getCreationImage: "TODO", // i have images as data URIs inside creations currently
+    getCreations: "https://api.iliazeus.lol/puddle/creations",
+  },
 ];
