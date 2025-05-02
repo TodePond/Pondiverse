11ded196db5a66a1a67cae3e4c4a42d8eb06b2e6
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-05-02 16:33:04 +0100

    metadata?

--- instances.js
+++ instances.js
@@ -3,7 +3,7 @@ export const instances = [
     name: "todepondiverse",
     domain: "pondiverse.com",
     addCreation: "https://pondiverse.val.run/add-creation",
-    getCreation: "https://pondiverse.val.run/get-creation/?id=",
+    getCreationMetadata: "https://pondiverse.val.run/get-creation/?id=",
     getCreationImage: "https://pondiverse.val.run/get-creation-image/?id=",
     getCreations: "https://pondiverse.val.run/get-creations",
   },
@@ -11,7 +11,7 @@ export const instances = [
     name: "puddle",
     domain: "iliazeus.lol",
     addCreation: "https://iliazeus-puddle.web.val.run/creations/",
-    getCreation: "https://iliazeus-puddle.web.val.run/creations/",
+    getCreationMetadata: "https://iliazeus-puddle.web.val.run/creations/",
     // getCreationImage: "TODO", // i have images as data URIs inside creations currently
     getCreations: "https://iliazeus-puddle.web.val.run/creations/",
   },
