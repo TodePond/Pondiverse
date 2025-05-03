8da6331a488c3924735d59e044c1254b08fb9bbf
Elouan Grimm <elouan.grimm@gmail.com>
2025-05-03 15:22:55 +0200

    added headers, wont fix

--- learn/example.js
+++ learn/example.js
@@ -123,6 +123,9 @@ function addPondiverseButton() {
                         new URL("/add-creation", PONDIVERSE_INSTANCE_URL),
                         {
                           method: "POST",
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
                           body: JSON.stringify(request),
                         }
                       );
